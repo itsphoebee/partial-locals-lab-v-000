a401019f0a5a9342c1b39082a56a57cb91842e8f
Phoebe Lau <phoebelau7@gmail.com>
2018-01-31 20:50:55 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -31,6 +31,6 @@ class StudentsController < ApplicationController
   end
 
   def student_params
-    params.require(:student).permit(:name, :birthday, :hometown, :name)
+    params.require(:student).permit(:name, :birthday, :hometown, [name:])
   end
 end
